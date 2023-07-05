56e1ebb5a4a58066bbcda69f17c71570d5a5c48a
Jacintamayol94 <jacimayol94@gmail.com>
2023-07-04 21:33:34 -0300

    ejemplos de pedidos con async-await

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -2,49 +2,69 @@ let db = require("../database/models")
 
 
 let peliculasController = {
-     crear: function(req, res){
-        db.Genero.findAll()
-            .then(function(generos){
-                return res.render("creacionPeliculas", {generos:generos})
-                
+    crear: async (req, res) => {
+        try {
+
+            const generos = await db.Genero.findAll({
+                raw: true
             })
-     },
-     guardado: function(req, res) {
-        db.Pelicula.create({
+
+            return res.render("creacionPeliculas", { generos: generos })
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    },
+    guardado: async (req, res) => {
+
+        const nuevaPelicula = {
             title: req.body.titulo,
             awards: req.body.premios,
             release_date: req.body.release_date,
             genre_id: req.body.genero,
             length: req.body.duracion,
             rating: req.body.rating,
+        };
+        try {
+            await db.Pelicula.create(nuevaPelicula)
+            res.redirect("/peliculas");
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    listado: async (req, res) => {
+        try {
+            const peliculas = await db.Pelicula.findAll({
+                raw: true
             })
-        res.redirect("/peliculas");
-     },
-     listado: function(req,res) {
-        db.Pelicula.findAll()
-            .then(function(peliculas){
-                res.render("listadoPeliculas", {peliculas:peliculas})
-            })
-     },
-     detalle: function(req,res){
+
+            res.render("listadoPeliculas", { peliculas: peliculas })
+
+        } catch (error) {
+            res.render("listadoPeliculas", { peliculas: [] })
+            console.log(error);
+        }
+    },
+    detalle: function (req, res) {
         db.Pelicula.findByPk(req.params.id, {
-            include: [{association: "genero"}, {association: "actores"}]
+            include: [{ association: "genero" }, { association: "actores" }]
         })
-            .then(function(pelicula) {
-                res.render("detallePelicula", {pelicula:pelicula})
+            .then(function (pelicula) {
+                res.render("detallePelicula", { pelicula: pelicula })
             })
-     },
-     editar: function(req, res){
+    },
+    editar: function (req, res) {
         let pedidoPelicula = db.Pelicula.findByPk(req.params.id);
 
         let pedidoGeneros = db.Genero.findAll();
 
         Promise.all([pedidoPelicula, pedidoGeneros])
-            .then(function([pelicula, generos]){
-                res.render("editarPelicula", {pelicula:pelicula, generos:generos})
+            .then(function ([pelicula, generos]) {
+                res.render("editarPelicula", { pelicula: pelicula, generos: generos })
             })
-     },
-     actualizar: function(req,res){
+    },
+    actualizar: function (req, res) {
         db.Pelicula.update({
             title: req.body.titulo,
             awards: req.body.premios,
@@ -52,22 +72,22 @@ let peliculasController = {
             genre_id: req.body.genero,
             length: req.body.duracion,
             rating: req.body.rating,
-            }, {
-        where: {
-            id: req.params.id
-        }
-    });
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
 
-    res.redirect("/peliculas/" + req.params.id)
+        res.redirect("/peliculas/" + req.params.id)
     },
-    borrar: function(req,res){
-    /*Primero borrar actores*/
+    borrar: function (req, res) {
+        /*Primero borrar actores*/
         db.Pelicula.destroy({
             where: {
                 id: req.params.id
             }
         })
-    res.redirect("/peliculas")
+        res.redirect("/peliculas")
     }
 }
 
